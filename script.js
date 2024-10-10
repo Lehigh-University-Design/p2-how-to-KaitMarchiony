55faff3af8dfe2c58d5fdb55cadbce40b861bc5b
Shirley Angos <shirleyangos@ShirleyMac-5.local>
2024-10-10 11:04:04 -0400

    fixed things with DOM and overlays

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-var burgerMenu = document.getElementById('burger-menu');
-var overlay = document.getElementById('menu');
-burgerMenu?.addEventListener('click',function(){
-    Console.log ("Hello world");
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+
+
+
+document.addEventListener('DOMContentLoaded', function() {  // Added this to the DOMContentLoaded event listener
+  var burgerMenu = document.getElementById('burger-menu');
+  var overlay = document.getElementById('menu');
+  burgerMenu?.addEventListener('click', function() {
+    console.log("Hello world");
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+  });
 });
