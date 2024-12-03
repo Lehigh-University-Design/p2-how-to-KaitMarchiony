a6da56d6bcf42e106be8e8cc7e769315af7561fa
KaitMarchiony <kim225@lehigh.edu>
2024-12-03 18:12:03 -0500

    update

--- script.js
+++ script.js
@@ -2,11 +2,6 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {  // Added this to the DOMContentLoaded event listener
-  var burgerMenu = document.getElementById('burger-menu');
-  var overlay = document.getElementById('menu');
-  burgerMenu?.addEventListener('click', function() {
-    console.log("Hello world");
-    this.classList.toggle("close");
-    overlay.classList.toggle("overlay");
-  });
+const targetDiv = document.getElementById()
+
 });
